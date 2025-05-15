5649caf91a0d64568033aac5cf294d92c608f847
ariannaeesposito <s311047@studenti.polito.it>
2025-05-15 14:53:51 +0200

    Update Utils.hpp

---
+++ Progetto2025/src/Utils.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include<vector>
+#include <iostream>
+#include "PolygonalMesh.hpp"
+#include "UCDUtilities.hpp"
+
+using namespace std;
+using namespace Gedim;
+
+namespace PolygonalLibrary
+{
+    bool ImportMesh(PolygonalMesh& mesh,const string& nomefile0,const string& nomefile1,const string& nomefile2);
+
+ 
+}

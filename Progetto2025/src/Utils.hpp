4cf11d844b5fc7e025d568d8dcbd8fb27c4b1563
ariannaeesposito <s311047@studenti.polito.it>
2025-05-16 14:21:29 +0200

    Update Utils.hpp

--- Progetto2025/src/Utils.hpp
+++ Progetto2025/src/Utils.hpp
@@ -10,8 +10,18 @@ using namespace Gedim;
 
 namespace PolygonalLibrary
 {
+    bool ImportMesh(PolygonalMesh& mesh,const string& nomefile0,const string& nomefile1,const string& nomefile2);
+    bool input_solido_platonico(PolygonalMesh& mesh, int dim ,char* argv[]);
 
-bool ImportMesh(PolygonalMesh& mesh);
-bool input_solido_platonico(PolygonalMesh& mesh, int dim ,char* argv[]);
+    bool ImportCell0Ds(PolygonalMesh& mesh);
+    bool ImportCell1Ds(PolygonalMesh& mesh);
+    bool ImportCell2Ds(PolygonalMesh& mesh);
+
+    void triangolazione(PolygonalMesh& mesh);
+    void triangolazione1(PolygonalMesh& mesh);
+    void triangolazione2(PolygonalMesh& mesh);
+
+    unsigned int accedimappa(TriangularMesh& mesh, PolygonalMesh& Pmesh, Vector3d<unsigned int> coord);
+ 
  
 }

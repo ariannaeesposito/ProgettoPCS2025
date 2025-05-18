f86e5e81bc678237d817442b659bd52b4c0301e9
Alfredo Giacomini <S311355@studenti.polito.it>
2025-05-18 11:42:39 +0200

    aggiunto main

--- Progetto2025/main.cpp
+++ Progetto2025/main.cpp
@@ -1,4 +1,45 @@
 int main()
 {
+	#include <iostream>
+	#include "Polygon.hpp"
+	#include "Utils.hpp"
+	#include "UCDUtilities.hpp"
+	
+	using namespace std;
+	using namespace Eigen;
+	using namespace PolygonalLibrary;
+	
+	int main(int dim ,char* argv[])
+	{
+		PolygonalMesh mesh;
+		cout << input_solido_platonico(mesh, dim, argv) << endl;
+	
+		if(!input_solido_platonico(mesh, dim, argv));
+		{
+			cerr << "Errorore" << endl;
+			return 1;
+		}
+	
+		if(!ImportMesh(mesh))
+		{
+			cerr << "file not found" << endl;
+			return 1;
+		}
+		else
+		{
+			cout << "File imported successfully" << endl;
+		}
+		
+		Gedim::UCDUtilities utilities;
+		utilities.ExportPoints("./Cell0Ds.inp",
+								mesh.M0D);
+		utilities.ExportSegments("./Cell1Ds.inp",
+								  mesh.M0D,
+								  mesh.M1D);
+	
+	
 	return 0;
-}
+	
+	}
+	
+	}

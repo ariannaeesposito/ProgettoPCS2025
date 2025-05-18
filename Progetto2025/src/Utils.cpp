917ca7ed57fb1b7291d1df07c54370854eefd399
ariannaeesposito <s311047@studenti.polito.it>
2025-05-18 14:00:40 +0200

    Update Utils.cpp con la mappa

--- Progetto2025/src/Utils.cpp
+++ Progetto2025/src/Utils.cpp
@@ -288,47 +288,55 @@ bool ImportCell2Ds(PolygonalMesh& mesh)
     return true;
     }
 
-// // void triangolazione(PolygonalMesh& mesh){
-// 	if (mesh.classe ==1){
-// 		triangolazione1(mesh);
-// 	}
-// 	else{
-// 		triangolazione2(mesh);
-// 	}
-// }
+//struct per comparare ( nella mappa srve qualcosa che abbia implementato in se loperatore di disuguaglianza)
 
-// void triangolazione1(PolygonalMesh& mesh){
-// 	unsigned int facce = mesh.Dim2D;
-// 	for (unsigned int i=0; i<facce; i++){ // i è la faccia
-// 		unsigned int Aid = mesh.M2D_vertici(i,0);
-// 		unsigned int Bid = mesh.M2D_vertici(i,1);
-// 		unsigned int Cid = mesh.M2D_vertici(i,2);
-// 		Vector3d Acoord = mesh.M0D.col(Aid);
-// 		Vector3d Bcoord = mesh.M0D.col(Bid);
-// 		Vector3d Ccoord = mesh.M0D.col(Cid);
-// 	}
-// }
+/*struct Vector3dComparator {
+    bool operator()(const Vector3d& a, const Vector3d& b) const {
+        for (int i = 0; i < 3; ++i) {
+            if (a[i] < b[i]) return true;
+            if (a[i] > b[i]) return false;
+        }
+        return false;
+    }
+};*/
 
-// void triangolazione2(PolygonalMesh& mesh){
-	
-// }
+unsigned int accedimappa(PoligonalLibrary::TriangularMesh& mesh, PolygonalMesh& Pmesh, Vector3d& coord){
 
-// unsigned int accedimappa(TriangularMesh& mesh, PolygonalMesh& Pmesh, Vector3d<unsigned int> coord){
-// 	double tol = pow(10,-15); //definiamo tolleranza
-// 	for (const auto& coppia : mesh.coordinate_punti){ // iteriamo sulla mappa
-// 		diff = coppia.first-coord;// calcoliamo la differnaza tra i due set di coordinate
-// 		if (diff.norm() < tol){ // se sono vicine allora le consideriamo le stesse
-// 			return coppia.second // restituiamo l'id associato a quelle coordinate
-// 		}
-// 	}
-// 	// se non c'è nella mappa lo creiamo nuovo
-// 	unsigned int dim = Pmesh.Dim0D; // dim corrente
-// 	mesh.coordinate_punti[coord] = dim+1; // cell0ds già piena stiamo, stiamo aggiungendo alla mappa una chiave e l'id relativo
-// 	Pmesh.dim0D += 1; // aumentiamo il conteggio dei punti già creati
-// 	Pmesh.M0D(0, dim+1)=coord(0); // mettiamo il nuovo punto dentro la polygonal mesh
-// 	Pmesh.M0D(1, dim+1)=coord(1);
-// 	Pmesh.M0D(2, dim+1)=coord(2);
-// 	return dim+1;
-//     }
+	double tol = pow(10,-15); //definiamo tolleranza
+
+    //controlliamo se gia presente il puntio 
+
+	for (const auto& coppia : mesh.coordinate_punti){ //iteriamo sulla mappa
+		double diff = (coppia.first - coord).norm();
+    //calcoliamo la differnaza tra i due set di coordinate
+		if (diff < tol){ //se sono vicine allora le consideriamo le stesse
+			return coppia.second;  //restituiamo l'id associato a quelle coordinate ( gia presente )
+		}
+	}
+	//se non c'è nella mappa lo creiamo nuovo 
+
+	unsigned int dim = Pmesh.Dim0D; 
+    mesh.coordinate_punti[coord] = dim;       // assegna nuovo ID al punto. Inserisce coord come nuova chiave nella mappa coordinate_punti, con valore dim (cioè l’ID).
+    Pmesh.Dim0D += 1;
+
+    Pmesh.M0D(0, dim) = coord(0);    //Salva le coordinate del nuovo punto nella matrice M0D
+    Pmesh.M0D(1, dim) = coord(1);
+    Pmesh.M0D(2, dim) = coord(2);
+
+    return dim;
+    
+    
+  /*unsigned int dim = Pmesh.Dim0D; //dim corrente
+
+	mesh.coordinate_punti[coord] = dim+1; //cell0ds già piena stiamo, stiamo aggiungendo alla mappa una chiave e l'id relativo
+	Pmesh.Dim0D += 1;  //aumentiamo il conteggio dei punti già creati
+	Pmesh.M0D(0, dim+1)=coord(0);  //mettiamo il nuovo punto dentro la polygonal mesh
+	Pmesh.M0D(1, dim+1)=coord(1);
+	Pmesh.M0D(2, dim+1)=coord(2);
+	return dim+1;*/
 }
+
+}
+
+
 
